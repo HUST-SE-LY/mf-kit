4537257d73269da161db165d6338f83728f73020
fenrisliu <fenrisliu@tencent.com>
2024-07-11 20:12:27 +0800

    bugfix: templates target file name

--- build.ts
+++ build.ts
@@ -1,4 +1,4 @@
 import path from 'node:path'
 import { copy } from './src/utils/copy'
 
-copy(path.join(__dirname, './templates'), path.join(__dirname, './dist/template'))
+copy(path.join(__dirname, './templates'), path.join(__dirname, './dist/templates'))
