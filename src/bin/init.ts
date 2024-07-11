832466200ff6b9956f6df152deb96238748d436c
fenrisliu <fenrisliu@tencent.com>
2024-07-11 20:50:04 +0800

    feat: change package.json name in init

--- src/bin/init.ts
+++ src/bin/init.ts
@@ -2,6 +2,7 @@ import { input, select } from '@inquirer/prompts';
 import { join } from 'path';
 import { templatePathMap } from '../constants';
 import { copy } from '../utils/copy';
+import { readFileSync, writeFileSync } from 'fs';
 
 export async function init() {
   const projectName = await input({ message: 'Please enter project name:' });
@@ -24,4 +25,7 @@ export async function init() {
   );
   const targetPath = join(cwd, projectName);
   copy(templatePath, targetPath);
+  const packageJson = JSON.parse(readFileSync(join(cwd, `./${projectName}`, '/package.json'), 'utf-8'));
+  packageJson.name = projectName;
+  writeFileSync(join(cwd, `./${projectName}`, '/package.json'), JSON.stringify(packageJson, null, 2));
 }
