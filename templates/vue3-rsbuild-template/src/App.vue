fd16654c4be049833a7894c3b300e6734adcf8e3
HUST-SE-LY <1596087252@qq.com>
2024-07-15 02:30:33 +0800

    feat: update vue3 template with router

--- templates/vue3-rsbuild-template/src/App.vue
+++ templates/vue3-rsbuild-template/src/App.vue
@@ -1,26 +1,11 @@
 <template>
-  <div class="background">
-    <div class="main">Vue + Module Federation 2.0 + Rsbuild</div>
-  </div>
-  </template>
+  <router-link></router-link>
+</template>
     
-  <script setup lang="ts">
+<script setup lang="ts">
   
-  </script>
+</script>
     
-  <style scoped>
-  .background {
-    width: 100vw;
-    height: 100vh;
-    background: rgba(0, 0, 0, 0.8);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .main {
-    color: white;
-    font-size: 36px;
-    font-weight: 700;
-  }
-  </style>
+<style scoped>
+
+</style>
